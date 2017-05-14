305eb65303d24c734ea9a70515d41d28ac3fe39b
Mark Kwon <hjkwon0609@gmail.com>
2017-05-13 23:19:38 -0700

    pull

--- client/link_generator.js
+++ client/link_generator.js
@@ -2,18 +2,30 @@
 
 // Create link that has info
 
-var create_link = function(pub_key, prv_key) {
-    API.get_user_object(pub_key, function(user) {
+var create_link = function(link_owner_pub_key, pub_key, prv_key) {
+    API.get_user_object(link_owner_pub_key, function(user) {
         console.log("<link_generator.js>");
-        let user_obj = user.value;
-        console.log(user_obj);
-        console.log(Object.keys(user_obj.info_loc));
+        var user_obj = user.value;
+        console.log(user_obj.info_loc);
+        var info_location = user_obj.info_loc;
+
+        var keys = Object.keys(info_location);
+
+        var ops = [];
+
+        for (var i = 0; i < keys.length; i++) {
+            var location = info_location[keys[i]];
+            var block_num = location[0];
+            var txn_hash = location[1];
+
+            ops.push(API.transaction.create_request_txn(prv_key, pub_key, block_num, txn_hash, link_owner_pub_key));
+        }
+
+        Promise.all(ops).then(function(results) {
+
+        });
 
 
-        // let keys = Object.keys(info_locations);
-        // for (let i = 0; i < keys.length; i++) {
-        //     console.log("Key: " + keys[i]);
-        // }
         // var results = User.process_serialized_txns_for_render(User.obj, prv_key);
         // var locations = [];
         // for (let i = 0; i < results.length; i++) {
