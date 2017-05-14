aa449bc8e608acac2e360499046e474c990d0a97
Mark Kwon <hjkwon0609@gmail.com>
2017-05-13 18:07:40 -0700

    pushing

--- client/link_generator.js
+++ client/link_generator.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log("Arrived");
+// Create link that has info
+
+var create_link = function(pub_key, prv_key) {
+    API.get_user_object(pub_key, function(user) {
+        console.log("<link_generator.js>");
+        let user_obj = user.value;
+        console.log(user_obj);
+        console.log(Object.keys(user_obj.info_loc));
+
+
+        // let keys = Object.keys(info_locations);
+        // for (let i = 0; i < keys.length; i++) {
+        //     console.log("Key: " + keys[i]);
+        // }
+        // var results = User.process_serialized_txns_for_render(User.obj, prv_key);
+        // var locations = [];
+        // for (let i = 0; i < results.length; i++) {
+        //     console.log(results[i]);
+        //
+        // }
+
+    });
+};
